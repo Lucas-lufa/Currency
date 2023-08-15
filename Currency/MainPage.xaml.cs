8e943bcab8a3ac093928d489ef39e2999c34c95b
faderl <faderl@L306-05>
2023-08-15 15:25:57 +0800

    Add project files.

---
+++ Currency/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Currency;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void plus1_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
+
---
+++ Currency/Money.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency
+{
+    
+    internal class Money
+    {
+        public int cents { get; set; }
+
+        public static void AddCents(int cents)
+        {
+
+        }
+    }
+}
